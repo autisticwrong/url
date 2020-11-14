99db6ad4d641a3979854ece47bcc2b4e5b0c8eb3
autisticwrong <43218868+autisticwrong@users.noreply.github.com>
2020-11-14 18:15:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from flask import Flask, redirect, render_template, request
+import random, string
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
   if request.method == "POST":
-    link = request.form['link']
+    #link = request.form['link']
     #return redirect(link)
-    return render_template("newlink.html", )
+    id = "".join(random.choices(string.ascii_lowercase + string.digits, k=6))
+    return render_template("newlink.html", link=id)
 
   if request.method == 'GET':
     return render_template("index.html")
 
 @app.route("/<id>")
 def redirect_url(id):
-  redirect("home")
+  #link = request.form['link']
+  #return redirect(link)
+  pass
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080, debug=True)
+  app.run(host='0.0.0.0', port=8080, debug=True)  app.run(host='0.0.0.0', port=8080, debug=True)
