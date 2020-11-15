3989cadf724d0abc91cdb8b3abe07fbc4e06c883
autisticwrong <43218868+autisticwrong@users.noreply.github.com>
2020-11-14 19:05:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from flask import Flask, redirect, render_template, request
-import random, string
+import random, string, json
 
 app = Flask(__name__)
+
+def data(id, link):
+  x = {
+      "data": {
+        
+        "id": id,
+        
+       "link": link
+      }
+    }
+
+  with open("url.json", "w") as f:
+    json.dump(x, f)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
   if request.method == "POST":
-    #link = request.form['link']
+    link = request.form['link']
     #return redirect(link)
+    
     id = "".join(random.choices(string.ascii_lowercase + string.digits, k=6))
+    data(id, link)
     return render_template("newlink.html", link=id)
 
   if request.method == 'GET':
@@ -18,7 +33,13 @@ def home():
 def redirect_url(id):
   #link = request.form['link']
   #return redirect(link)
-  pass
+  with open("url.json", "r") as f:
+    data = json.load(f)
+    if data["data"]["id"] == id:
+      url = data["data"]["link"]
+      print(url)
+    #return render_template("index.html")
+    return redirect(url)
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080, debug=True)  app.run(host='0.0.0.0', port=8080, debug=True)
+  app.run(host='0.0.0.0', port=8080, debug=True), port=8080, debug=True)
